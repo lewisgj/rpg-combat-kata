092abf5f00654229d4a485ee7359b8742467bbe8
Lewis Greenway-Jones <you@example.com>
2022-01-09 10:57:49 +0000

    Iteration 1, Step 1-ish: Green damage test

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,1 +1,33 @@
+export class Character {
+    #health = 1000;
+    #isAlive = true;
 
+    isAlive(): boolean {
+        return this.#isAlive;
+    }
+
+    dealDamage(other: Character, damage: number) {
+        other.#health -= damage;
+        if (other.#health < 0) {
+            other.#health = 0;
+            other.#isAlive = false;
+        }
+    }
+}
+
+describe("Characters", () =>{
+    it('should start alive', () => {
+        const character = new Character();
+
+        expect(character.isAlive()).toBeTruthy();
+    });
+
+    it('should die after 1000 damage', () => {
+        const character1 = new Character();
+        const character2 = new Character();
+
+        character1.dealDamage(character2, 1001);
+        expect(character2.isAlive()).toBeFalsy();
+    });
+
+})
